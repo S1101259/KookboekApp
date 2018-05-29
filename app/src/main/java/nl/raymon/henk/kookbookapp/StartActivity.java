759aff96f7dba66c79f573af63e7836e1d903f99
Testman3 <testman3@hotmail.com>
2018-05-29 15:51:44 +0200

    Added initial activities

---
+++ app/src/main/java/nl/raymon/henk/kookbookapp/StartActivity.java
@@ -0,0 +1,40 @@
+package nl.raymon.henk.kookbookapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class StartActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
+        setContentView(R.layout.activity_start);
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+//        updateUI(currentUser);
+    }
+
+    public void goLogin(View v) {
+        startActivity(new Intent(this, LoginActivity.class));
+    }
+
+    public void goRegister(View v) {
+        startActivity(new Intent(this, RegisterActivity.class));
+    }
+}
